9768af28bbc9b10a0661d2a4256f8780d0ab1ae7
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-12-04 23:29:06 +0100

    v0.0.31 add typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -25,13 +26,14 @@ module.exports = {
       }
     ]
   },
+  plugins: [new CopyPlugin([{ from: './index.d.ts', to: './main.d.ts' }])],
   externals: {
     react: {
       root: 'React',
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react',
-      umd: 'react',
+      umd: 'react'
     }
   }
 };
